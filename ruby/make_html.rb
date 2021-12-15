ef63d0e4cf3f829eef5fea443958ceff4d42dc26
kurokky_mac <>
2021-12-15 16:54:03 +0900

    no message

--- ruby/make_html.rb
+++ ruby/make_html.rb
@@ -6,33 +6,45 @@ class Main
 	def initialize
 		@diff_types = ["🆕","⬆️", "⬇️", "➡️"]
 	end
-	def make_ranking(date = "20211210")
-		json = File.read("./json/#{date}_ranking.json",encodung:'utf-8')
+	def make_ranking(date = "20211203", ignore_regends = false)
+		if (ignore_regends)
+			file_name = "#{date}__" 
+		else
+			file_name = "#{date}_"
+		end
+		json = File.read("./json/#{file_name}ranking.json",encodung:'utf-8')
 		json = JSON.parse(json)
 		h1 = json['date']
 		list = []
+		n = 1
 		json['ranking'].each do |li|
 			html = '<li>'
 			html += "#{@diff_types[li['diff_type']]}"
-			html += '<i>' + li['ranking'].to_s + '</i>'
+			if ignore_regends
+				html += '<i>' + n.to_s + '</i>'
+				n += 1 
+			else
+				html += '<i>' + li['ranking'].to_s + '</i>'
+			end
 			html += '<a href="https://www.figma.com/community/plugin/' + li["plugin_id"].to_s + '">'
 			html += li['plugin_name'] + '</a>'
-			html += ' <b>(' + li['diff_count'].to_s + 'DL)</b>'
+			html += ' <b>(' + li['diff_count'].to_s.reverse.gsub( /(\d{3})(?=\d)/, '\1,').reverse + 'DL)</b>'
 			html += '</li>'
 			list.push(html)
 		end
+
 		base = File.read("./template/temp.html",encodung:'utf-8')
 		temp = base.gsub('<!--title-->',"Figma Weekly Ranking #{date}")
 		temp = temp.gsub('<!--h1-->',"#{date}")
 		temp = temp.gsub('<!--ul-->',list.join("\n"))
-		File.open("./#{date}.html","w") do |f|
+		File.open("./#{file_name}.html","w") do |f|
 			f.puts(temp)
 		end
 	end
 end
 
 
-main = Main.new().make_ranking()
+main = Main.new().make_ranking(20211210, true)
 
 
 
